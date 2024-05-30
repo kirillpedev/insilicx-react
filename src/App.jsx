c974265c0693908a482b6bc8f19eea5ed5ddb077
kirillpedev <kirillpetrov2511@gmail.com>
2024-05-30 18:05:38 +0300

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Home from './pages/Home'
+import Reviews from './pages/Reviews'
 
 const App = () => {
   return (
-    <Home/>
+    <Reviews/>
   ) 
 }
 
--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Main_Reviews from '../components/main_reviews'
+import MainReviews from '../components/MainReviews'
 
 const Reviews = () => {
   return (
     <div>   
       <Header/>
-      <Main_Reviews/>
+      <MainReviews/>
       <Footer/>
     </div>
 )
