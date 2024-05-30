6af5038ce481d379882e0c91586a73d17540b193
kirillpedev <kirillpetrov2511@gmail.com>
2024-05-30 13:02:07 +0300

    Ginger commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import vksvg from '../svg/vk.svg'
+import telegramsvg from '../svg/telegram.svg'
+import youtubesvg from '../svg/youtube.svg'
 
 const FooterMain = styled.div`
     font-family: "Anonymous Pro", monospace;
@@ -9,40 +12,101 @@ const FooterMain = styled.div`
     color: white;
     padding-left: 250px;
     padding-right: 250px;
+
+    .footer-container {
+        display: flex;
+        justify-content: space-between;
+        padding-top: 4vh;
+    }
+
+    .caption {
+        font-size: 24px;
+        color: rgba(255, 255, 255, 0.3);
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+    }
+`
+
+const LogoLinks = styled.div`
+    p {
+        font-size: 20px;
+        margin-top: 23px;
+        color: rgba(255, 255, 255, 0.3);
+    }
+
+    .icons img {
+        margin-right: 12px;
+        margin-top: 27px;
+    }
+`
+
+const ShopNav = styled.div`
+    font-size: 18px;
+
+    p {
+        line-height: 35px;
+    }
+`
+
+const InfoNav = styled.div`
+    font-size: 18px;
+
+    p {
+        line-height: 35px;
+    }
+`
+
+const BuisnessNav = styled.div`
+    font-size: 16px;
+
+    p {
+        font-size: 15px;
+        color: rgba(255, 255, 255, 0.3);
+        margin-top: 5px;
+        /* line-height: 9px; */
+    }
+
+    h3 {
+        font-size: 20px;
+        margin-top: 15px;
+    }
 `
 
 const Footer = () => {
   return (
-    <div className='footer-main'>
+    <FooterMain>
         <div className='footer-container'>
-            <div className='logo-links'>
+            <LogoLinks>
                 <h1>Insilicx inc.</h1>
                 <div className='icons'>
-                    <img src="" width={30} height={30}/>
-                    <img src="" width={30} height={30}/>
-                    <img src="" width={30} height={30}/>
+                    <a href='#'><img src={vksvg} width={30} height={30}/></a>
+                    <a href='#'><img src={telegramsvg} width={30} height={30}/></a>
+                    <a href='#'><img src={youtubesvg} width={30} height={30}/></a>
                 </div>
                 <p>© 2024 Insilicx inc.</p>
-            </div>
-            <div className='shop-nav'>
+            </LogoLinks>
+            <ShopNav>
                 <p className='caption'>МАГАЗИН</p>
-                <p>Каталог</p>
-                <p>И так далее</p>
-            </div>
-            <div className='info-nav'>
+                <p><a href='#'>Каталог</a></p>
+                <p><a href='#'>И так далее</a></p>
+            </ShopNav>
+            <InfoNav>
                 <p className='caption'>ИНФОРМАЦИЯ</p>
-                <p>Доставка и оплата</p>
-                <p>Публичная оферта</p>
-                <p>Контакты</p>
-            </div>
-            <div className='buisness-nav'>
+                <p><a href='#'>Доставка и оплата</a></p>
+                <p><a href='#'>Публичная оферта</a></p>
+                <p><a href='#'>Контакты</a></p>
+            </InfoNav>
+            <BuisnessNav>
                 <p>ИП: Захаров Михаил Дмитриевич</p>
                 <p>ИНН: 744410767676</p>
                 <p>ОГРНИП: 317745600008299</p>
                 <h3>shop@insilicx.ru</h3>
-            </div>            
-            </div>
-    </div>
+            </BuisnessNav>            
+        </div>
+    </FooterMain>
   )
 }
 
