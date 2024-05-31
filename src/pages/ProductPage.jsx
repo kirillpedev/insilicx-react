68cf4f787804b1ead8ae7a09c692bce0608616d5
dan <danilakitaev596@gmail.com>
2024-05-31 15:14:22 +0300

    Support added for all pages except:ErrorPage.

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ProductPageMain from '../components/ProductPageMain'
+import Support from '../components/Support'
 
 const ProductPage = () => {
   return (
     <div>   
       <Header/>
+      <Support/>
       <ProductPageMain/>
       <Footer/>
     </div>
--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import MainReviews from '../components/MainReviews'
+import Support from '../components/Support'
 
 const Reviews = () => {
   return (
     <div>   
       <Header/>
+      <Support/>
       <MainReviews/>
       <Footer/>
     </div>
