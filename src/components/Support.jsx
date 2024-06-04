25129d6f5c40f9cd02745131a310841d4e03804a
Cybersport <Cybersport@26kadr.internal>
2024-06-04 15:49:11 +0300

    added chat alpha ver.

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -2,17 +2,93 @@ import React from 'react'
 import styled from 'styled-components'
 import shoppingbag from '../svg/shopping-bag.svg'
 import headsetsvg from '../svg/headset.svg'
+import pedofil from '/src/img/pedofil.jpg'
+
+const Chat = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+    .chat{
+    right: -12px; /* Расстояние от правого края окна браузера */
+    top: 30%;
+    position: absolute;
+    display: none;
+    width: 260px;
+    height: 655px;
+    background-color: #0C0C0C;
+    border-radius: 15px;
+    }
+    & img {
+        border-radius: 50px;
+    }
+
+    .chat h1 {
+        margin-top: 10%;
+        color: #fff;
+        text-align: center;
+        font-family: "Anonymous Pro", monospace;
+        font-size: 30px;
+        font-weight: bold;
+    }
+
+    .chat h3 {
+        margin-top: 25%;
+        color: #ffffff30;
+        text-align: center;
+        font-family: "Anonymous Pro", monospace;
+        font-size: 32px;
+        font-weight: bold;
+    }
+`
+
+const Message = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+
+  
+    .message {
+        display: flex;
+        background-color: #212121;
+        width: 152px;
+        height: 40px;
+        margin-top: 140%;
+        margin-left: 5%;
+        color: #fff;
+        font-family: "Noto Sans", sans-serif;
+        font-weight: bold;
+        border-radius: 21px;
+    }
+
+    .message p {
+        /* margin-top: 15px; */
+        padding-left: 2px;
+        color: #ffffff3b;
+        font-family: "Noto Sans", sans-serif;
+        font-size: 10px;
+        font-weight: bold;
+    }
+
+    .message h2 {
+        /* margin-top: 15px; */
+        padding-left: 8px;
+        color: #fff;
+        font-family: "Noto Sans", sans-serif;
+        font-size: 10px;
+        font-weight: bold;
+    }
+`
 
 const Button = styled.div`
+    .btn_pos{
+        position: fixed; /* Фиксированное положение */
+        display: grid;
+        grid-row-gap: 25px;
+        right: 90px; /* Расстояние от правого края окна браузера */
+        top: 70%; /* Расстояние сверху */
+    }
 
-    position: fixed; /* Фиксированное положение */
-    display: grid;
-    grid-row-gap: 25px;
-    right: 60px; /* Расстояние от правого края окна браузера */
-    top: 70%; /* Расстояние сверху */
+   
 
     .round-button {
-        width: 70px; height: 70px;
+        width: 70px; 
+        height: 70px;
         background-color: white;
         border-radius: 50%;
         border: none;
@@ -25,12 +101,27 @@ const Button = styled.div`
 export default function Support() {
   return (
     <Button>
-        <button className='round-button'>
-            <img src={shoppingbag} width={38} height={38} alt=""/>
-        </button>
-        <button className='round-button'>
-            <img src={headsetsvg} alt=""/>
-        </button>
+        <Chat>
+            <div className='chat'>
+                <h1>Что случилось сын берёзы?</h1>
+                <Message>
+                    <div className='message'>
+                    <img src={pedofil} alt="" width="45" height="45"/>
+                        <p>Advokat666<h2>Я сру. Хули надо?</h2></p>
+                    </div>  
+                </Message> 
+                <h3>Функция чата заблокирована.</h3>
+            </div>
+        </Chat>
+        <div className='btn_pos'>
+            <button className='round-button'>
+                <img src={shoppingbag} width={38} height={38} alt=""/>
+            </button>
+            <button className='round-button'>
+                <img src={headsetsvg} alt=""/>
+            </button>
+        </div>
+        
     </Button>
   )
 }
